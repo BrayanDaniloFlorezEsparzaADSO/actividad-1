72eb9eba6b5b8a17fa1d09eacf82173fddee0ee1
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-09 19:15:02 -0500

    11

---
+++ 11.js
@@ -0,0 +1,14 @@
+addEventListener(`DOMContentLoaded`,()=>{
+  // # DEFINIR VARIABLES NUEMRIAS
+  // # DATOS DE ENTRADA
+  // ## OPERACIONES BASICAS: + /*
+  let div=Number(prompt(`ingrese en numero`,4/3))
+  let pi=Number(prompt(`ingrese el numero`,3.14))
+  let A=Number(prompt(`dijite el valor de A:`))
+  let B=Number(prompt(`dijite el valor de B:`))
+  let C=Number(prompt(`dijite el valor de C:`))
+   // #PROCESOS U OPERACIONES
+  let v=(div)*pi+A*B*C
+  // #salida o entrada de resultado
+  console.log(`algoritmo para obtener una elipsoide:${v}`);
+})
