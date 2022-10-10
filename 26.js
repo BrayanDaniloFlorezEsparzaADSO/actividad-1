8aa8d0a3ff64f78bea5ecd37fb02c494987b4bc7
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-09 19:37:06 -0500

    26

---
+++ 26.js
@@ -0,0 +1,11 @@
+addEventListener(`DOMContentLoaded`,()=>{
+    // # DEFINIR VARIABLES| NUEMRIAS
+    // # DATOS DE ENTRADA|
+    // ## OPERACIONES BASICAS:| + /
+    let cajas=Number(25)
+    let kilos=Number(748)
+    // #PROCESOS U OPERACIONES
+    let res=(cajas*kilos)
+    // #salida o entrada de resultado
+    console.log(`numero de kilos que transporta: ${res} kg`);
+})
