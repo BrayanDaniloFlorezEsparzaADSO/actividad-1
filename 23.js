42fe5bcb8aef97480fb054e9e938625293e0407c
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-09 19:33:35 -0500

    23

---
+++ 23.js
@@ -0,0 +1,11 @@
+addEventListener(`DOMContentLoaded`,()=>{
+    // # DEFINIR VARIABLES NUEMRIAS
+    // # DATOS DE ENTRADA
+    // ## OPERACIONES BASICAS: + /
+    let madre=Number(prompt(`edad de la Mamá:`))
+    let hijo=Number(prompt(`edad del hijo:`))
+    // #PROCESOS U OPERACIONES
+    let res=(madre-hijo)
+    // #salida o entrada de resultado
+    console.log(`edad de la madre en el parto: ${res}`);
+})
