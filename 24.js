1602d12d352554b6ca3ce90264699d7b8b274e43
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-09 19:34:40 -0500

    24

---
+++ 24.js
@@ -0,0 +1,15 @@
+addEventListener(`DOMContentLoaded`,()=>{
+    // # DEFINIR VARIABLES NUEMRIAS
+// # DATOS DE ENTRADA
+// ## OPERACIONES BASICAS: + /
+let llavesh=Number(11500) 
+let bomba=Number(1168000) 
+let pernos=Number(87000) 
+let cambio=Number(91000) 
+// #PROCESOS U OPERACIONES
+let res1=(llavesh*5)
+let res2=(pernos*3)
+let res3=(res1+res2+bomba+cambio)
+// #salida o entrada de resultado
+console.log(`total de dinaro que tenia:${res3}`);
+})
