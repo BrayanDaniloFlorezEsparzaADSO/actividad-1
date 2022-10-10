4be64f81f1b97eaa9b39e0c9f3ffb1006163c513
BrayanDaniloFlorezEsparzaADSO1~ <brayandanilo2004@gmail.com>
2022-10-09 19:38:46 -0500

    27

---
+++ 27.js
@@ -0,0 +1,12 @@
+addEventListener(`DOMContentLoaded`,()=>{
+    // # DEFINIR VARIABLES NUEMRIAS
+    // # DATOS DE ENTRADA
+    // ## OPERACIONES BASICAS: + /
+    // from fractions import Fraction
+    let martin=fractions(2**3)
+    let jairo=fractions(1**4)
+    // #PROCESOS U OPERACIONES
+    let res=(martin-jairo)
+    // #salida o entrada de resultado
+    console.log(` la parte qeu le responde a lorena es de: ${res}`);
+})
